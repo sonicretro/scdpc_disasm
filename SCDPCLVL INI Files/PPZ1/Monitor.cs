00f0febc06f4595398aa5e68b16f0ea653942634
unknown <devnull@localhost>
2011-08-05 03:06:47 +0000

    MainMemory - SonLVL: Apparently Sonic CD PC doesn't store loop flags in the layout. Also updating objdefs.

---
+++ SCDPCLVL INI Files/PPZ1/Monitor.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using SonicRetro.SonLVL;
+
+namespace SCDPCObjectDefinitions.Common
+{
+    class Monitor : ObjectDefinition
+    {
+        private Point offset;
+        private BitmapBits img;
+        private int imgw, imgh;
+		private BitmapBits postimg;
+		private Point postoff;
+        private List<Point> offsets = new List<Point>();
+        private List<BitmapBits> imgs = new List<BitmapBits>();
+        private List<int> imgws = new List<int>();
+        private List<int> imghs = new List<int>();
+
+        public override void Init(Dictionary<string, string> data)
+        {
+            img = ObjectHelper.Sprite(286, out offset);
+            imgw = img.Width;
+            imgh = img.Height;
+			postimg = ObjectHelper.Sprite(285, out postoff);
+            Point off;
+            BitmapBits im;
+            for (int i = 0; i < 8; i++)
+            {
+				im = ObjectHelper.Sprite(272 + i, out off);
+                imgs.Add(im);
+                offsets.Add(off);
+                imgws.Add(im.Width);
+                imghs.Add(im.Height);
+            }
+            for (int i = 0; i < 2; i++)
+            {
+				im = ObjectHelper.Sprite(281 + i, out off);
+                imgs.Add(im);
+                offsets.Add(off);
+                imgws.Add(im.Width);
+                imghs.Add(im.Height);
+            }
+        }
+
+        public override ReadOnlyCollection<byte> Subtypes()
+        {
+            return new ReadOnlyCollection<byte>(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        }
+
+        public override string Name()
+        {
+            return "Monitor/Timepost";
+        }
+
+        public override bool RememberState()
+        {
+            return true;
+        }
+
+        public override string SubtypeName(byte subtype)
+        {
+            switch (subtype)
+            {
+                case 0:
+                    return "Sonic Monitor";
+                case 1:
+                    return "Rings Monitor";
+                case 2:
+                    return "Shield Monitor";
+                case 3:
+                    return "Invincibility Monitor";
+                case 4:
+                    return "Speed Shoes Monitor";
+                case 5:
+                    return "Clock Monitor";
+                case 6:
+                    return "Silver Ring Monitor";
+                case 7:
+                    return "S Monitor";
+                case 8:
+                    return "Past Timepost";
+                case 9:
+                    return "Future Timepost";
+                default:
+                    return "Unknown";
+            }
+        }
+
+        public override string FullName(byte subtype)
+        {
+            return SubtypeName(subtype);
+        }
+
+        public override BitmapBits Image()
+        {
+            return img;
+        }
+
+        public override BitmapBits Image(byte subtype)
+        {
+            if (subtype <= 9)
+                return imgs[subtype];
+            else
+                return img;
+        }
+
+        public override Rectangle Bounds(Point loc, byte subtype)
+        {
+            if (subtype <= 7)
+                return Rectangle.Union(new Rectangle(loc.X + offset.X, loc.Y + offset.Y, imgw, imgh), new Rectangle(loc.X + offsets[subtype].X, loc.Y + offsets[subtype].Y, imgws[subtype], imghs[subtype]));
+            else if (subtype <= 9)
+                return Rectangle.Union(new Rectangle(loc.X + postoff.X, loc.Y + postoff.Y, postimg.Width, postimg.Height), new Rectangle(loc.X + offsets[subtype].X, loc.Y + offsets[subtype].Y, imgws[subtype], imghs[subtype]));
+			else
+                return new Rectangle(loc.X + offset.X, loc.Y + offset.Y, imgw, imgh);
+        }
+
+        public override void Draw(BitmapBits bmp, Point loc, byte subtype, bool XFlip, bool YFlip, bool includeDebug)
+        {
+            if (subtype <= 7 | subtype > 9)
+            {
+    			BitmapBits bits = new BitmapBits(img);
+	            bits.Flip(XFlip, YFlip);
+                bmp.DrawBitmapComposited(bits, new Point(loc.X + offset.X, loc.Y + offset.Y));
+				if (subtype <= 7)
+				{
+				    bits = new BitmapBits(imgs[subtype]);
+    	            bits.Flip(XFlip, YFlip);
+                    bmp.DrawBitmapComposited(bits, new Point(loc.X + offsets[subtype].X, loc.Y + offsets[subtype].Y));
+				}
+		    }
+			else
+			{
+    			BitmapBits bits = new BitmapBits(postimg);
+	            bits.Flip(XFlip, YFlip);
+                bmp.DrawBitmapComposited(bits, new Point(loc.X + postoff.X, loc.Y + postoff.Y));
+    		    bits = new BitmapBits(imgs[subtype]);
+                bits.Flip(XFlip, YFlip);
+                bmp.DrawBitmapComposited(bits, new Point(loc.X + offsets[subtype].X, loc.Y + offsets[subtype].Y));
+    		}
+		}
+
+        public override Type ObjectType
+        {
+            get
+            {
+                return typeof(MonitorSCDObjectEntry);
+            }
+        }
+    }
+
+    public class MonitorSCDObjectEntry : SCDObjectEntry
+    {
+        public MonitorSCDObjectEntry() : base() { }
+        public MonitorSCDObjectEntry(byte[] file, int address) : base(file, address) { }
+
+        public MonitorType Type
+        {
+            get
+            {
+                return (MonitorType)(SubType & 8);
+            }
+            set
+            {
+                SubType = (byte)((SubType & ~8) | (int)value);
+            }
+        }
+		
+		public enum MonitorType
+		{
+		    Monitor = 0,
+			Timepost = 8
+		}
+    }
+}
---
+++ SCDPCLVL INI Files/PPZ1/Spring.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using SonicRetro.SonLVL;
+
+namespace SCDPCObjectDefinitions.PPZ1
+{
+    class Spring : ObjectDefinition
+    {
+        private List<Point> offsets = new List<Point>();
+        private List<BitmapBits> imgs = new List<BitmapBits>();
+
+        public override void Init(Dictionary<string, string> data)
+        {
+            Point off = new Point();
+            imgs.Add(ObjectHelper.Sprite(479, out off)); // 0x00
+            offsets.Add(off);
+            imgs.Add(ObjectHelper.Sprite(467, out off)); // 0x02
+            offsets.Add(off);
+            imgs.Add(ObjectHelper.Sprite(482, out off)); // 0x10
+            offsets.Add(off);
+            imgs.Add(ObjectHelper.Sprite(470, out off)); // 0x12
+            offsets.Add(off);
+            imgs.Add(ObjectHelper.Sprite(485, out off)); // 0x20
+            offsets.Add(off);
+            imgs.Add(ObjectHelper.Sprite(473, out off)); // 0x22
+            offsets.Add(off);
+            imgs.Add(imgs[0]); // 0x30
+            offsets.Add(offsets[0]);
+            imgs.Add(imgs[1]); // 0x32
+            offsets.Add(offsets[1]);
+        }
+
+        public override ReadOnlyCollection<byte> Subtypes()
+        {
+            return new ReadOnlyCollection<byte>(new byte[] { 0x00, 0x02, 0x10, 0x12, 0x20, 0x22 });
+        }
+
+        public override string Name()
+        {
+            return "Spring";
+        }
+
+        public override bool RememberState()
+        {
+            return false;
+        }
+
+        public override string SubtypeName(byte subtype)
+        {
+            return ((SpringDirection)((subtype & 0x30) >> 4)).ToString() + " " + ((SpringColor)((subtype & 2) >> 1)).ToString();
+        }
+
+        public override string FullName(byte subtype)
+        {
+            return SubtypeName(subtype) + " " + Name();
+        }
+
+        public override BitmapBits Image()
+        {
+            return imgs[0];
+        }
+
+        private int imgindex(byte subtype)
+        {
+            int result = (subtype & 2) >> 1;
+            result |= (subtype & 0x30) >> 3;
+            return result;
+        }
+
+        public override BitmapBits Image(byte subtype)
+        {
+            return imgs[imgindex(subtype)];
+        }
+
+        public override Rectangle Bounds(Point loc, byte subtype)
+        {
+            return new Rectangle(loc.X + offsets[imgindex(subtype)].X, loc.Y + offsets[imgindex(subtype)].Y, imgs[imgindex(subtype)].Width, imgs[imgindex(subtype)].Height);
+        }
+
+        public override void Draw(BitmapBits bmp, Point loc, byte subtype, bool XFlip, bool YFlip, bool includeDebug)
+        {
+            BitmapBits bits = new BitmapBits(imgs[imgindex(subtype)]);
+            bits.Flip(XFlip, YFlip);
+            bmp.DrawBitmapComposited(bits, new Point(loc.X + offsets[imgindex(subtype)].X, loc.Y + offsets[imgindex(subtype)].Y));
+        }
+
+        public override Type ObjectType
+        {
+            get
+            {
+                return typeof(SpringSCDObjectEntry);
+            }
+        }
+    }
+
+    public class SpringSCDObjectEntry : SCDObjectEntry
+    {
+        public SpringSCDObjectEntry() : base() { }
+        public SpringSCDObjectEntry(byte[] file, int address) : base(file, address) { }
+
+        public SpringDirection Direction
+        {
+            get
+            {
+                return (SpringDirection)((SubType & 0x30) >> 4);
+            }
+            set
+            {
+                SubType = (byte)((SubType & ~0x30) | ((int)value << 4));
+            }
+        }
+
+        public SpringColor Color
+        {
+            get
+            {
+                return (SpringColor)((SubType & 2) >> 1);
+            }
+            set
+            {
+                SubType = (byte)((SubType & ~2) | ((int)value << 1));
+            }
+        }
+    }
+
+    public enum SpringDirection
+    {
+        Vertical,
+        Horizontal,
+        Diagonal,
+        Invalid
+    }
+
+    public enum SpringColor
+    {
+        Red,
+        Yellow
+    }
+}
